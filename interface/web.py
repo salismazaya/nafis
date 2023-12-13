a56c22a53fa2863c0515983989104c6be9095b3e
billalxcode <billal.xcode@gmail.com>
2023-12-13 19:06:15 +0700

    update logging in web.py

--- interface/web.py
+++ interface/web.py
@@ -1,3 +1,6 @@
+import logging
+from rich.logging import RichHandler
+from rich.console import Console
 import gradio as gr
 from lib.chroma import collection
 from lib.predict import predict, predict_image
@@ -5,12 +8,31 @@ from lib.schema import PredictResult
 from PIL import Image
 import os
 
+console = Console()
+logging.basicConfig(
+    level="NOTSET",
+    format="%(message)s",
+    datefmt="[%X]",
+    handlers=[RichHandler()]
+)
+logger = logging.getLogger("rich")
+logger.setLevel(logging.INFO)
+
+for log_name, log_object in logging.Logger.manager.loggerDict.items():
+    if log_name != "rich":
+        log_object.disabled = True
+
 def prediction(image_array):
     result = predict_image(Image.fromarray(image_array))
-    if isinstance(result, PredictResult):
-        return f"{result.name} {result.hours}:{result.minutes}:{result.seconds} Score: {result.score}"
+    if result is None:
+        logger.warning("No result found!")
+        return "No result found!"
     else:
-        return "No result found"
+        logger.info(f"The film has been found")
+        logger.info(f"Movie Name: {result.name}")
+        logger.info(f"Estimated Duration: {result.hours}:{result.minutes}:{result.seconds}")
+        logger.info(f"Score: {result.score}")
+        return f"{result.name} {result.hours}:{result.minutes}:{result.seconds} Score: {result.score}"
     
 def start_web():
     demo = gr.Interface(fn = prediction, inputs = "image", outputs = "text")
