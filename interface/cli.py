b46e6efef580698d019b4ccad3ed7d9f3ceee569
billalxcode <billal.xcode@gmail.com>
2023-12-13 19:04:42 +0700

    update logging in cli

--- interface/cli.py
+++ interface/cli.py
@@ -1,17 +1,40 @@
 import os
 import sys
+import logging
+from rich.logging import RichHandler
+from rich.console import Console
 from lib.chroma import collection
 from lib.predict import predict, predict_image
 from PIL import Image
 
+console = Console()
+logging.basicConfig(
+    level="NOTSET",
+    format="%(message)s",
+    datefmt="[%X]",
+    handlers=[RichHandler()]
+)
+logger = logging.getLogger("rich")
+logger.setLevel(logging.INFO)
+
+for log_name, log_object in logging.Logger.manager.loggerDict.items():
+    if log_name != "rich":
+        log_object.disabled = True
+        
 def start_cli():
-    filename = sys.argv[2]
+    try:
+        filename = sys.argv[2]
+    except IndexError:
+        filename = console.input("Enter your file: ")
     if os.path.isfile(filename) is not True:
-        print ("File no such found")
+        logger.error(f"The {filename} file could not be found")
         
     image = Image.open(filename)
     result = predict_image(image=image)
     if result is None:
-        print ("No result found")
+        logger.warning("No result found!")
     else:
-        print (result)
+        logger.info(f"The film has been found")
+        logger.info(f"Movie Name: {result.name}")
+        logger.info(f"Estimated Duration: {result.hours}:{result.minutes}:{result.seconds}")
+        logger.info(f"Score: {result.score}")
