29cd04734475df53ba577d061854f3bc28937393
billalxcode <billal.xcode@gmail.com>
2023-12-14 14:54:13 +0700

    feat: design pattern

--- embed.py
+++ embed.py
@@ -7,83 +7,130 @@ from pathlib import Path
 import os, glob, hashlib, pickle, zlib, sys
 from lib.console import Console
 
-console = Console()
-if not os.path.exists('outputs'):
-    console.warning("Output directory not found")
-    console.info("Create new outputs directory")
-    os.mkdir('outputs')
+class ImageSpliter:
+    def __init__(self, output_folder: str, video_path: str, console: Console) -> None:
+        self.output_folder = output_folder
+        self.video_path = video_path
+        self.console = (console if console is None else Console())
 
-def split_video_to_images(video_path, output_folder, start_on = 0, interval_seconds = 1):
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
+        self.clip = VideoFileClip(self.video_path)
 
-    clip = VideoFileClip(video_path)
-    n_frames = clip.reader.nframes
-    duration = clip.duration
-    console.info(f"Movie duration: {(round(duration / 3600) if duration > 3600 else duration)} hour")
+    @property
+    def duration(self):
+        return self.clip.duration
     
-    timestamps = range(start_on, int(duration), interval_seconds)
-    def execute(i):
-        console.info(f"Take frame {i}/{len(timestamps)}")
-        frame = Image.fromarray(clip.get_frame(i))
-        image_path = os.path.join(output_folder, f"{i}.png")
-        frame.save(image_path)
-    
-    console.info(f"Takes image frames from a total of {n_frames} frames")
-    for i in timestamps:
-        execute(i)
-
-    clip.reader.close()
-
-try:
-    WORKER = int(sys.argv[2])
-except IndexError:
-    WORKER = int(input('Enter Total Worker: '))
-
-console.info(f"Your cpu count: {os.cpu_count()}")
-if WORKER > os.cpu_count():
-    console.warning("[yellow]Not recommended: [reset]if the number of workers is greater than the number of CPUs", extra={"markup": True})
-    
-files = glob.glob('videos/*.mkv') + glob.glob('videos/*.mp4')
-databases = glob.glob('database/*')
-outputs = glob.glob('outputs/*')
-
-def start(file: str):
-    path = Path(file)
-    title = path.as_posix().split('/')[-1]
-    console.info("Initiate image separation from video")
-    console.info(f"{file} started!")
-    file_checksum = hashlib.md5(open(file, 'rb').read()).hexdigest()
-    output_folder = 'outputs/' + file_checksum
-    if output_folder in outputs:
-        total_file = len(glob.glob(f'{output_folder}/*.png'))
-        split_video_to_images(file, output_folder, start_on = total_file)
-    else:
-        split_video_to_images(file, output_folder)
-    
-    if not 'database/' + file_checksum + '.nafis' in databases:
-        console.info(f'Embedding {file} started!')
+    def timestamps(self, start_on: int = 0, interval_seconds: int = 1):
+        timestamps = range(start_on, int(self.duration), interval_seconds)
+        return timestamps
         
+    def check_output(self):
+        if not os.path.exists(self.output_folder):
+            os.makedirs(self.output_folder)
+
+    def execute(self, index: int):
+        frame = Image.fromarray(
+            self.clip.get_frame(index)
+        )
+        image_path = os.path.join(self.output_folder, f"{index}.png")
+        frame.save(image_path)
+
+    def run(self, start_on: int = 0, interval_seconds: int = 1):
+        self.check_output()
+        self.console.info(f'Movie duration: {(round(self.duration))}')
+        for index in self.timestamps(start_on, interval_seconds):
+            self.execute(index=index)
+        self.clip.reader.close()
+
+class Embed:
+    def __init__(self) -> None:
+        self.console = Console()
+        self.supported_extension = ["mkv", "mp4"]
+
+        self.files = self.get_files()
+        self.databases = self.get_databases()
+        self.outputs = self.get_outputs()
+
+        self.check_output()
+
+    def get_files(self):
+        files = []
+        for ext in self.supported_extension:
+            files.extend(
+                glob.glob(f'videos/*.{ext}')
+            )
+        return files
+    
+    def get_databases(self):
+        databases = glob.glob('databases/*')
+        return databases
+    
+    def get_outputs(self):
+        outputs = glob.glob('outputs/*')
+        return outputs
+    
+    def check_output(self):
+        if not os.path.exists('outputs'):
+            self.console.warning("Output directory not found")
+            self.console.info("Create new outputs directory")
+            os.mkdir('outputs')
+
+    def embed(self, output_folder: str, title: str = "", checksum: str = ""):
         embeddings = []
-        images = glob.glob(output_folder + '/*.png')
+        images = glob.glob(output_folder + "/*.png")
         for image in images:
             timestamp = Path(image).as_posix().split('/')[-1].removesuffix('.png')
-            timestamp = int(timestamp)
-            image_obj = Image.open(image)
-            vector = predict(image_obj)
-            
-            embeddings.append(Embedding(title = title, timestamp = timestamp, vector = vector))
-        
-        result = Result(title = title, embeddings = embeddings)
-        with open('database/' + file_checksum + '.nafis', 'wb') as f:
-            console.info("Write the embedding results")
-            f.write(
-                zlib.compress(
-                    pickle.dumps(result)
+            image_object = Image.open(image)
+            vector = predict(image_object)
+            embeddings.append(
+                Embedding(
+                    title=title,
+                    timestamp=int(timestamp),
+                    vector=vector
                 )
             )
+        
+        result = Result(
+            title=title,
+            embeddings=embeddings
+        )
+        with open(os.path.join('database', checksum + ".nafis"), 'wb') as File:
+            File.write(
+                zlib.compress(pickle.dumps(result))
+            )
 
-with console.status("[bold green] Starting embedding system") as status:
-    with ThreadPoolExecutor(max_workers = WORKER) as t:
-        t.map(start, files)
-    
+    def start(self, filename: str):
+        self.console.info("Initialite image seperation from video")
+        self.console.info(f"{filename} started!")
+        
+        path = Path(filename)
+        title = path.as_posix().split("/")[-1]
+        checksum = hashlib.md5(open(filename, "rb").read()).hexdigest()
+        output = os.path.join("outputs", checksum)
+        
+        image_spliter = ImageSpliter(output_folder=output, video_path=filename, console=self.console)
+        if os.path.isdir(output):
+            self.console.info("Continuing frame splitting")
+            start_on = len(glob.glob(f"{output}/*.png"))
+            image_spliter.run(start_on=start_on, interval_seconds=1)
+        else:
+            image_spliter.run()
+        
+        if os.path.isfile(os.path.join("database", checksum + '.nafis')):
+            self.console.info(f"Embedding {filename} started!")
+            self.embed(output_folder=output, title=title, checksum=checksum)
+
+    def main(self):
+        worker = self.console.input("Total worker: ")
+        if worker.rstrip().lstrip() == "":
+            worker = os.cpu_count()
+        if worker.isdigit() is not True:
+            self.console.danger("Input only accept digit")
+            sys.exit()
+        
+        with self.console.status("[bold green] Starting embedding system") as status:
+            with ThreadPoolExecutor(max_workers=int(worker)) as thread:
+                thread.map(self.start, (self.files))
+
+if __name__ == "__main__":
+    app = Embed()
+    app.main()
