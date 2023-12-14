fb7d335f048054b4d65bf1d7a0a244f6a10d1336
billalxcode <billal.xcode@gmail.com>
2023-12-14 12:33:48 +0700

    feat: implement design pattern

--- app.py
+++ app.py
@@ -3,21 +3,26 @@ import optparse
 from interface.cli import start_cli
 from interface.web import start_web
 
-def main():
-    parser = optparse.OptionParser()
-    parser.add_option("-i", "--interface", dest="interface", help="Application interface, default: web")
-    parser.add_option("-f", "--filename", dest="filename", help="Your file image")
-    parser.add_option("--cuda", dest="cuda", help="Use GPU", action="store_true")
-    options, args = parser.parse_args()
-    
-    if options.interface is None:
-        options.interface = "web"
-    if options.interface == "web":
-        start_web(options)
-    elif options.interface == "cli":
-        start_cli(options, args)
-    else:
-        parser.print_help()
+class App:
+    def __init__(self) -> None:
+        self.parser = optparse.OptionParser()
 
+    def configure(self):
+        self.parser.add_option("-i", "--interface", dest="interface", help="Application interface, default: web")
+        self.parser.add_option("-f", "--filename", dest="filename", help="Your file image")
+        self.parser.add_option("--cuda", dest="cuda", help="Use GPU", action="store_true")
+
+    def main(self):
+        options, args = self.parser.parse_args()
+        if options.interface is None:
+            options.interface = "web"
+        if options.interface == "web":
+            start_web(options)
+        elif options.interface == "cli":
+            start_cli(options, args)
+        else:
+            self.parser.print_help()
+     
 if __name__ == "__main__":    
-    main()
+    app = App()
+    app.main()
